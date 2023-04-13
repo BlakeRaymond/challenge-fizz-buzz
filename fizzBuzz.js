60b537a93f7d46a057e02c4166b43cf3c3188fad
Blake Raymond <blake.crypto@technologist.com>
2023-04-13 09:56:14 -0600

    fizzBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,12 @@
+// Write your solution below this line:
+for (let i=1; i<=50; i++){
+    if(i % 15 === 0){
+    console.log("fizzBuzz" + " ")
+} else if (i % 3 === 0) {
+    console.log("Fizz" + " ")
+} else if (i % 5 === 0) {
+    console.log("Buzz" + " ")
+} else {
+    console.log(i)
+}
+}
